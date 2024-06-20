bb0fe6ccd3d0b5580b0c5c7d784421bce24a319e
emilien <emilien.bancheri@imerir.com>
2024-06-20 09:42:43 +0200

    api ok requete getNote

--- PYTHON/api_distante.py
+++ PYTHON/api_distante.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
 import sqlite3
+import logging
 
 app = Flask(__name__)
-DATABASE = 'C:\\Users\\Thomas\\Documents\\BddNote.db'# Chemin vers votre base de données SQLite
+#DATABASE = 'C:\\Users\\emili\\Documents\\master2\\Tempest\\tempest_loc\\SQL\\BddNote.db'
+DATABASE = 'C:\\Users\\Thomas\\Documents\\BddNote.db'
+
+# Configurez le logging
+logging.basicConfig(level=logging.DEBUG)
 
 def get_db():
     conn = sqlite3.connect(DATABASE)
@@ -15,14 +20,18 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     conn.close()
+    logging.debug(f"Executed query: {query}")
+    logging.debug(f"With args: {args}")
+    logging.debug(f"Returned rows: {rv}")
     return (rv[0] if rv else None) if one else rv
 
-# Endpoint pour récupérer les notes d'un élève
 @app.route('/api/eleves/notes', methods=['GET'])
 def get_eleve_notes():
     prenom = request.args.get('prenom')
     nom = request.args.get('nom')
     classe_nom = request.args.get('classe')
+
+    logging.debug(f"Reçu les paramètres - Prenom: {prenom}, Nom: {nom}, Classe: {classe_nom}")
 
     eleve = query_db('''
         SELECT e.ID, e.Prenom, e.Nom, c.Nom as Classe
@@ -32,7 +41,10 @@ def get_eleve_notes():
     ''', [prenom, nom, classe_nom], one=True)
 
     if not eleve:
+        logging.debug("Eleve non trouvé")
         return jsonify({'erreur': 'Eleve non trouve'}), 404
+
+    logging.debug(f"Elève trouvé - ID: {eleve['ID']}, Prenom: {eleve['Prenom']}, Nom: {eleve['Nom']}, Classe: {eleve['Classe']}")
 
     notes = query_db('''
         SELECT n.Notes, m.Nom as Matiere, p.Nom as ProfesseurNom, p.Prenom as ProfesseurPrenom
@@ -41,6 +53,8 @@ def get_eleve_notes():
         JOIN Professeur p ON n.ProfID = p.ID
         WHERE n.EleveID = ?
     ''', [eleve['ID']])
+
+    logging.debug(f"Notes récupérées: {notes}")
 
     notes_data = [{'matiere': note['Matiere'], 'professeur': f"{note['ProfesseurPrenom']} {note['ProfesseurNom']}", 'note': note['Notes']} for note in notes]
 
